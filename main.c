c614a6e69f0b256477a30be64bdb319b1ba70b47
BarGoldman <bar8770@gmail.com>
2021-12-14 20:14:02 +0200

    version 4

--- main.c
+++ main.c
@@ -3,34 +3,44 @@
 #include "str.h"
 
 
-
 int main()
 {
-	
-	//char txt[] = "I’m bringing home my baby bumble bee Won’t my Mommy be so proud of me I’m bringing home my baby bumble bee – OUCH!! It stung me!!~";
-	char my_string[] = "bee"; 
-	//func1(my_string,txt);
-	
-	
-	char txt[] = "vvrv yv yvvm vvy~";
-	int length = strlen(my_string);
-	//printf("your str: %c\n",my_string[0]);
-	/*
-	for(int i = 0 ; i < length ; i++)
-	{
-		printf("temp : %c\n",my_string[i]);
-	}
-	*/
-	printf("%s\n",txt);
-	func2(my_string,txt);
-	/*
-	for(int i = 0 ; i < length ; i++)
-	{
-		printf("temp : %c\n",my_string[i]);
-	}
-	*/
-	return 0;
+    char word[30];
+    memset(word,0,strlen(word));
+    scanf("%c",&word[0]);
+    int i=1;
+    while(i<30)
+    {
+        scanf("%c",&word[i]);
+        if(word[i]==' ' || word[i]=='\n' || word[i]=='\t')
+        {
+            break;
+        }
+        i++;
+    }
+     char txt[1024];
+    memset(txt,0,strlen(txt));
+    scanf("%c",&txt[0]);
+    i=1;
+    while (i<1024)
+    {
+        scanf("%c",&txt[i]);
+        if(txt[i]=='~')
+        {
+            break;
+        }
+        i++;
+    }
+    printf("Gematria Sequences:");
+    func1("bee","I’m bringing home my baby bumble bee Won’t my Mommy be so proud of me I’m bringing home my baby bumble bee – OUCH!! It stung me!!~");
+    printf("Atbash Sequences:");
+    //func2("bee","vvrv yv yvvm vvy~");
+    printf("Anagram Sequences:");
+    //func3(word,txt);
+    
+    return 0;
 }
+
 	
 
 
